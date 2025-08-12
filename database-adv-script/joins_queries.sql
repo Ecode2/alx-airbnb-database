e76d2e78ce31c7da5b4d2e7813bfbc97f46abb8e
Ecode <ecode5814@gmail.com>
2025-08-12 11:49:39 +0100

    added review order

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -5,6 +5,7 @@ INNER JOIN Users ON Bookings.user_id = Users.id
 
 SELECT * FROM Properties
 LEFT JOIN Reviews ON Properties.id = Reviews.property_id
+ORDER BY Reviews.rating DESC
 
 
 SELECT * FROM Users
